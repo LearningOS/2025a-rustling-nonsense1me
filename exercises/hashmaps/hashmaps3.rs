949baa0dc20abd9a65fa73b84664c9e18e010068
nonsense1me <2207988212@qq.com>
2025-12-22 11:02:10 +0000

    update

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,12 +14,12 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
 struct Team {
+    name:String,
     goals_scored: u8,
     goals_conceded: u8,
 }
@@ -39,6 +39,32 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        if let Some(team) =scores.get_mut(&team_1_name){
+            team.goals_scored += team_1_score;
+            team.goals_conceded += team_2_score;
+        }else{
+            scores.insert(
+                team_1_name.clone(),
+                Team{
+                    name:team_1_name,
+                    goals_scored: team_1_score,
+                    goals_conceded:team_2_score,
+                }
+            );
+        }
+        if let Some(team) = scores.get_mut(&team_2_name){
+            team.goals_scored += team_2_score;
+            team.goals_conceded += team_1_score;
+        }else{
+            scores.insert(
+                team_2_name.clone(),
+                Team{
+                    name:team_2_name,
+                    goals_scored:team_2_score,
+                    goals_conceded:team_1_score,
+                }
+            );
+        }
     }
     scores
 }
--- exercises/lifetimes/lifetimes3.rs
+++ exercises/lifetimes/lifetimes3.rs
@@ -4,14 +4,10 @@
 //
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a
 // hint.
-
-// I AM NOT DONE
-
-struct Book {
-    author: &str,
-    title: &str,
+struct Book<'a> {
+    author: &'a str,
+    title: &'a str,
 }
-
 fn main() {
     let name = String::from("Jill Smith");
     let title = String::from("Fish Flying");
